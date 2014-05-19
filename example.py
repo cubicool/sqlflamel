9edfce957899072bb13ade5fc2cf8733dca8cd4e
Jeremy Moles <cubicool@gmail.com>
2014-05-19 10:11:22 -0400

    Convert to PEP8 formatting.

--- example.py
+++ example.py
@@ -1,107 +1,117 @@
-# This is an example of SQLFlamel usage in a project that defines two ORM objects
-# and their corresponding tables: User and Hours. A Hours object is a span of time
-# in which the User worked.
+# This is an example of SQLFlamel usage in a project that defines two ORM
+# objects and their corresponding tables: User and Hours. A Hours object is a
+# span of time in which the User worked.
 
 import sqlflamel
 import sqlalchemy.ext.declarative
 import datetime
 
-from sqlalchemy     import Column, Enum, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, Enum, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, backref
+
 
 Base = sqlalchemy.ext.declarative.declarative_base()
 
+
 class User(Base):
-	__tablename__ = "users"
+    __tablename__ = "users"
 
-	BROADCAST = "on", "off", "online"
+    BROADCAST = "on", "off", "online"
 
-	id        = Column(Integer, primary_key=True)
-	node      = Column(String(256), nullable=False)
-	domain    = Column(String(255), nullable=False)
-	broadcast = Column(Enum(*BROADCAST), default=BROADCAST[0])
+    id = Column(Integer, primary_key=True)
+    node = Column(String(256), nullable=False)
+    domain = Column(String(255), nullable=False)
+    broadcast = Column(Enum(*BROADCAST), default=BROADCAST[0])
 
-	# Here we derive from QueryProxy and define a single convenience method: from_jid.
-	# This lets us have code like the following:
-	#
-	# user = User.Proxy(db.query(Users)).from_jid()
-	#
-	# HOWEVER! That syntax alone isn't any more or less unweildy than the default SQLAlchemy
-	# syntax, so we combine this technique with our custom Database class.
-	class Proxy(sqlflamel.QueryProxy):
-		def from_jid(self, jid):
-			node, domain = jid.split("@")
+    # Here we derive from QueryProxy and define a single convenience method:
+    # from_jid.  This lets us have code like the following:
+    #
+    # user = User.Proxy(db.query(Users)).from_jid()
+    #
+    # HOWEVER! That syntax alone isn't any more or less unweildy than the
+    # default SQLAlchemy syntax, so we combine this technique with our custom
+    # Database class.
+    class Proxy(sqlflamel.QueryProxy):
+        def from_jid(self, jid):
+            node, domain = jid.split("@")
 
-			return self._query.filter_by(node=node, domain=domain).one()
+            return self._query.filter_by(node=node, domain=domain).one()
+
 
 class Hours(Base):
-	__tablename__ = "hours"
+    __tablename__ = "hours"
 
-	id      = Column(Integer, primary_key=True)
-	user_id = Column(Integer, ForeignKey(User.id), nullable=False)
-	start   = Column(DateTime)
-	end     = Column(DateTime)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    start = Column(DateTime)
+    end = Column(DateTime)
 
-	user = relationship(User, backref=backref(__tablename__, order_by=id))
+    user = relationship(User, backref=backref(__tablename__, order_by=id))
 
-	class Proxy(sqlflamel.QueryProxy):
-		def between(self, start, end):
-			return self._query.filter(Hours.start.between(start, end)).all()
+    class Proxy(sqlflamel.QueryProxy):
+        def between(self, start, end):
+            return self._query.filter(Hours.start.between(start, end)).all()
+
 
 class Database(sqlflamel.Database):
-	OBJECTS = User, Hours
+    @staticmethod
+    def objects():
+        return [User, Hours]
+
 
 if __name__ == "__main__":
-	# A sqlflamel Database object handles all of the syntactical binding between the user-defined
-	# ORM objects and the sessions it creates and manages.
-	database = Database("sqlite://")
+    # A sqlflamel Database object handles all of the syntactical binding
+    # between the user-defined ORM objects and the sessions it creates and
+    # manages.
+    database = Database("sqlite://")
 
-	# Add a User and commit it right away.
-	with database.create_context() as db:
-		db.add(User(node="cubicool", domain="github.com"))
+    # Add a User and commit it right away.
+    with database.create_context() as db:
+        db.add(User(node="cubicool", domain="github.com"))
 
-	# Add some Hours, and commit those right away too.
-	with database.create_context() as db:
-		# Here we use our Proxy from_jid method. The Proxy notices that the Query object
-		# (which is what is natively returned from SQLAlchemy) doesn't contain a from_jid attribute,
-		# and instead finds the appropriate method in the Proxy type itself. The Proxy instance
-		# has access to the Query instance, such that each attribute access is actually a query
-		# evaluation underneath.
-		user = db.users.from_jid("cubicool@github.com")
+    # Add some Hours, and commit those right away too.
+    with database.create_context() as db:
+        # Here we use our Proxy from_jid method. The Proxy notices that the
+        # Query object (which is what is natively returned from SQLAlchemy)
+        # doesn't contain a from_jid attribute, and instead finds the
+        # appropriate method in the Proxy type itself. The Proxy instance has
+        # access to the Query instance, such that each attribute access is
+        # actually a query evaluation underneath.
+        user = db.users.from_jid("cubicool@github.com")
 
-		# We synthetically generate some Hours objects, saying we worked every day for 8 hours
-		# for the last 10 days. I don't personally recommend working 10 days in a row, but such
-		# is life when you're the admin, web site, server, and client application developer all
-		# in one. :)
-		now = datetime.datetime.now()
+        # We synthetically generate some Hours objects, saying we worked every
+        # day for 8 hours for the last 10 days. I don't personally recommend
+        # working 10 days in a row, but such is life when you're the admin, web
+        # site, server, and client application developer all in one. :)
+        now = datetime.datetime.now()
 
-		for i in range(10):
-			start = now - datetime.timedelta(i)
-			end   = start + datetime.timedelta(0, 60 * 60 * 8)
+        for i in range(10):
+            start = now - datetime.timedelta(i)
+            end = start + datetime.timedelta(0, 60 * 60 * 8)
 
-			db.add(Hours(user_id=user.id, start=start, end=end))
+            db.add(Hours(user_id=user.id, start=start, end=end))
 
-	# Here we just demonstrate some more syntactic sugar, which is really the only thing SQLFlamel
-	# currently tries to address. :)
-	with database.create_context() as db:
-		# The following are all the same row in the users table, but since db.users access is
-		# actually a property that issues a query dynamically, they are all unique instances.
-		# This is a double-edged sword! SQLFlamel provides some syntactic sugar, but you need
-		# to be aware of this to prevent youself from issuing multiple queries over and over for
-		# what is conceptually the same data.
-		print(db.users.all()[0])
-		print(db.users.one())
-		print(db.users.filter_by(id=1).all()[0])
-		print(db.users.from_jid("cubicool@github.com"))
+    # Here we just demonstrate some more syntactic sugar, which is really the
+    # only thing SQLFlamel currently tries to address. :)
+    with database.create_context() as db:
+        # The following are all the same row in the users table, but since
+        # db.users access is actually a property that issues a query
+        # dynamically, they are all unique instances.  This is a double-edged
+        # sword! SQLFlamel provides some syntactic sugar, but you need to be
+        # aware of this to prevent youself from issuing multiple queries over
+        # and over for what is conceptually the same data.
+        print(db.users.all()[0])
+        print(db.users.one())
+        print(db.users.filter_by(id=1).all()[0])
+        print(db.users.from_jid("cubicool@github.com"))
 
-		# Here we will once again use the convenience of the Proxy object to simplify our
-		# querying and SQLAlchemy usage.
-		now  = datetime.datetime.now().date()
-		then = now - datetime.timedelta(5)
+        # Here we will once again use the convenience of the Proxy object to
+        # simplify our querying and SQLAlchemy usage.
+        now = datetime.datetime.now().date()
+        then = now - datetime.timedelta(5)
 
-		# The "between" method here is provided by our Hours.Proxy object. Like from_jid
-		# above, it is used when SQLFlamel determines that between() doesn't exist on the
-		# Query object returned by SQLAlchemy.
-		for hours in db.hours.between(then, now):
-		 	print(hours.start, hours.end)
-
+        # The "between" method here is provided by our Hours.Proxy object. Like
+        # from_jid above, it is used when SQLFlamel determines that between()
+        # doesn't exist on the Query object returned by SQLAlchemy.
+        for hours in db.hours.between(then, now):
+            print(hours.start, hours.end)
--- sqlflamel.py
+++ sqlflamel.py
@@ -2,91 +2,95 @@ import sqlalchemy
 import sqlalchemy.orm
 import contextlib
 
-# This is a base class (intended to be derived from in each ORM object) that acts as
-# a kind of proxy between the SQLAlchemy query results object and the the client
-# interface allowing for much cleaner--though still necessarily dynamic--syntax.
+
+# This is a base class (intended to be derived from in each ORM object) that
+# acts as a kind of proxy between the SQLAlchemy query results object and the
+# the client interface allowing for much cleaner--though still necessarily
+# dynamic--syntax.
 class QueryProxy:
-	def __init__(self, query):
-		self._query = query
+    def __init__(self, query):
+        self._query = query
 
-	def __getattr__(self, attr):
-		if hasattr(self._query, attr):
-			return getattr(self._query, attr)
+    def __getattr__(self, attr):
+        if hasattr(self._query, attr):
+            return getattr(self._query, attr)
 
-		return getattr(self, attr)
+        return getattr(self, attr)
 
-# The Database class acts as a base class, and relies on having a sequence called
-# OBJECTS defined somewhere in the namespace resolved by accessing self. This list
-# should contain each ORM object that should exposed in session instances created by
-# this Database. Those tables will be available as attributes, and a new SQLAlchemy
-# query will be issued with each access.
+
+# The Database class acts as a base class, and relies on having a static method
+# called objects() defined somewhere in the namespace resolved by accessing
+# self. This list should contain each ORM object that should exposed in session
+# instances created by this Database. Those tables will be available as
+# attributes, and a new SQLAlchemy query will be issued with each access.
 class Database:
-	def __init__(self, engine):
-		self._engine = sqlalchemy.create_engine(engine)
+    def __init__(self, engine):
+        self._engine = sqlalchemy.create_engine(engine)
 
-		for obj in self.OBJECTS:
-			obj.metadata.create_all(self._engine)
+        for obj in self.objects():
+            obj.metadata.create_all(self._engine)
 
-		self._session = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker())
+        self._session = sqlalchemy.orm.scoped_session(
+            sqlalchemy.orm.sessionmaker()
+        )
 
-		self._session.configure(bind=self._engine)
+        self._session.configure(bind=self._engine)
 
-	# Creates a session object that must be manually closed and commited; you generally do not
-	# want to use this directly.
-	def create_session(self):
-		session = self._session()
+    # Creates a session object that must be manually closed and commited; you
+    # generally do not want to use this directly.
+    def create_session(self):
+        session = self._session()
 
-		# FINALLY! Here is where we combine the Proxy objects above with some really
-		# mastferful hackery. This block of code iterates over every ORM type in
-		# the self.OBJECTS sequence and sets a property on the type itself that lets you
-		# issue an SQLAlchemy query simply using attribute-access syntax. Further,
-		# that attribute is itself a Proxy object which, when the desired attribute
-		# or method isn't found in the native query result object (i.e., filter(),
-		# filter_by(), etc.), pushes the __getattr__ request up into the Proxy as a
-		# fallback.
-		#
-		# All of this nastiness combined lets us achieve the final syntactic goal of
-		# the following:
-		#
-		# database = Database("sqlite://foo.sql3")
-		#
-		# with database.create_context() as db:
-		# 	db.users.all()
-		# 	db,users.filter_by(id=1)
-		# 	db.users.my_method(1, 10)
-		#
-		# Notice how we never have to issue db.query(User) explicitly, and also note how,
-		# when db.users.my_method isn't found in the normal SQLAlchemy Query results object,
-		# the lookup is then propogated upwards into the Proxy object, where a matching method
-		# IS found and used.
-		#
-		# In each case, accessing db.users actually calls a property function that evaluates
-		# db.query(User) for us, and instantiates a Proxy using those results.
-		for obj in self.OBJECTS:
-			setattr(
-				type(session),
-				obj.__tablename__,
-				property(lambda self, obj=obj: obj.Proxy(self.query(obj)))
-			)
+        # FINALLY! Here is where we combine the Proxy objects above with some
+        # really mastferful hackery. This block of code iterates over every ORM
+        # type in the self.objects() sequence and sets a property on the type
+        # itself that lets you issue an SQLAlchemy query simply using
+        # attribute-access syntax. Further, that attribute is itself a Proxy
+        # object which, when the desired attribute or method isn't found in the
+        # native query result object (i.e., filter(), filter_by(), etc.),
+        # pushes the __getattr__ request up into the Proxy as a fallback.
+        #
+        # All of this nastiness combined lets us achieve the final syntactic
+        # goal of the following:
+        #
+        # database = Database("sqlite://foo.sql3")
+        #
+        # with database.create_context() as db: db.users.all()
+        # db,users.filter_by(id=1) db.users.my_method(1, 10)
+        #
+        # Notice how we never have to issue db.query(User) explicitly, and also
+        # note how, when db.users.my_method isn't found in the normal
+        # SQLAlchemy Query results object, the lookup is then propogated
+        # upwards into the Proxy object, where a matching method IS found and
+        # used.
+        #
+        # In each case, accessing db.users actually calls a property function
+        # that evaluates db.query(User) for us, and instantiates a Proxy using
+        # those results.
+        for obj in self.objects():
+            setattr(
+                type(session),
+                obj.__tablename__,
+                property(lambda self, obj=obj: obj.Proxy(self.query(obj)))
+            )
 
-		return session
+        return session
 
-	# Creates a context-compatible object that can be used with the special "with" keyword.
-	# This is almost always what you want to use. :)
-	@contextlib.contextmanager
-	def create_context(self):
-		session = self.create_session()
+    # Creates a context-compatible object that can be used with the special
+    # "with" keyword.  This is almost always what you want to use. :)
+    @contextlib.contextmanager
+    def create_context(self):
+        session = self.create_session()
 
-		try:
-			yield session
+        try:
+            yield session
 
-			session.commit()
+            session.commit()
 
-		except:
-			session.rollback()
+        except:
+            session.rollback()
 
-			raise
+            raise
 
-		finally:
-			session.close()
-
+        finally:
+            session.close()
